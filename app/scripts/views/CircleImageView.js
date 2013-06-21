885a507a26df011226213120626e27f483f463fa
Drew Vergara <anvergara@am.sony.com>
2013-06-20 18:27:44 -0700

    Adding in animations

--- app/scripts/views/CircleImageView.js
+++ app/scripts/views/CircleImageView.js
@@ -19,12 +19,12 @@ define(['jquery', 'underscore', 'backbone', 'paper', 'hammer'], function ($, _, 
         render: function (image) {
             var raster = new Raster(image);
             
-            raster.size = new Size(160, 90);
+            raster.size = new Size(20, 15);
             raster.visible = false;
 
             var gridSize = 12;
 
-            var spacing = 1.2;
+            var spacing = 1.0;
 
             for (var y = 0; y < raster.height; y++) {
                 for(var x = 0; x < raster.width; x++) {
@@ -46,8 +46,22 @@ define(['jquery', 'underscore', 'backbone', 'paper', 'hammer'], function ($, _, 
             project.activeLayer.position = view.center;
             project.activeLayer.fitBounds(view.bounds, true);
             paper.view.draw();
-
-            this.$el.hammer();
+            
+            paper.view.onFrame = this.onFrame;
+            this.$el.hammer();            
+        },
+        
+        onFrame: function () {
+            for (var i = 0; i < project.activeLayer.children.length; i++) {
+                var item = project.activeLayer.children[i];
+                item.fillColor.hue += 1;
+                
+//                item.position.x += item.bounds.width / 5;
+//        
+//                if (item.bounds.left > 224) {
+//                    item.position.x = -(item.bounds.width);
+//                }
+            }
         },
         
         events: {
